da256ceee40acec054aadee82a4b5f24842a8107
juan cruz <jcrnietos@gmail.com>
2023-09-25 21:41:56 -0300

    se agrgan imagenes a content y se agrega botton de ver productos

--- src/components/contentHome/ContentHome.jsx
+++ src/components/contentHome/ContentHome.jsx
@@ -1,26 +1,51 @@
 import './contentHome.css';
-import img_teclado_content from '../../image/img-teclado-content.jpg';
-import img_camaras_content from '../../image/img-camaras-content.jpg';
-import img_comunicador_content from '../../image/img-comunicador-content.jpg';
-import img_accesorios_content from '../../image/img-accesorios-content.jpg';
+import { GiRingingAlarm } from 'react-icons/gi';
+import { MdOutlineSecurity } from 'react-icons/md';
+import { MdOutlineSystemSecurityUpdateGood } from 'react-icons/md';
+import { MdOutlineSensorWindow } from 'react-icons/md';
+
 
 const ContentHome = () => {
-  return (
-   <div className='container-content-card'>
-        <div className='container-alarmas'>
-            <img src={img_teclado_content} alt="" className='img-content'/> 
-        </div>
-        <div className='container-camaras'>
-           <img src={img_camaras_content} alt="" className='img-content'/> 
-        </div>
-        <div className='container-accesorios'>
-           <img src={img_accesorios_content} alt="" className='img-content'/> 
-        </div>
-        <div className='container-comunicadores'>
-           <img src={img_comunicador_content} alt="" className='img-content'/> 
-        </div>
-    </div>
-  )
+   return (
+      <div className='container-content-card'>
+         <div className='container-alarmas'>
+            <div className='content-card'>
+               <i className='icon-content'> <GiRingingAlarm/></i>
+               <p className='text-content'>Kit de Alarmas</p>
+               <div className='container-buttom-content'>
+                 <button>Ver Productos</button>
+               </div>
+            </div>
+         </div>
+         <div className='container-camaras'>
+            <div className='content-card'>
+               <i className='icon-content'><MdOutlineSecurity/></i>
+               <p className='text-content'>Kit de Cámaras</p>
+               <div className='container-buttom-content'>
+                 <button>Ver Productos</button>
+               </div>
+            </div>
+         </div>
+          <div className='container-accesorios'>
+            <div className='content-card'>
+               <i className='icon-content'><MdOutlineSensorWindow/></i>
+               <p className='text-content'>Accesirios para Alármas</p>
+               <div className='container-buttom-content'>
+                 <button>Ver Productos</button>
+               </div>
+            </div>
+         </div>
+         <div className='container-comunicadores'>
+            <div className='content-card'>
+               <i className='icon-content'><MdOutlineSystemSecurityUpdateGood/></i>
+               <p className='text-content'>Comunicador para Alarmas</p>
+               <div className='container-buttom-content'>
+                 <button>Ver Productos</button>
+               </div>
+            </div>
+         </div> 
+      </div>
+   )
 }
 
 export default ContentHome;
